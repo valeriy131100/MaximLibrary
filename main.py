42541836e80e2f3ef0f57bc41a1e88816bc65866
Valery Efremov <valeriy131100@gmail.com>
2022-02-02 10:32:26 +0300

    Catch redirect while download books

--- main.py
+++ main.py
@@ -3,14 +3,25 @@ import os
 import requests
 
 
+def check_for_redirect(response: requests.Response):
+    if not response.history:
+        return
+
+    raise requests.HTTPError
+
+
 def download_books(count, folder_path):
     book_url = 'http://tululu.org/txt.php?id={book_id}'
 
     for book_id in range(1, count + 1):
-        response = requests.get(
-            book_url.format(book_id=book_id)
-        )
-        response.raise_for_status()
+        try:
+            response = requests.get(
+                book_url.format(book_id=book_id)
+            )
+            response.raise_for_status()
+            check_for_redirect(response)
+        except requests.HTTPError:
+            continue
 
         book_filename = f'{book_id}.txt'
         book_filepath = os.path.join(folder_path, book_filename)

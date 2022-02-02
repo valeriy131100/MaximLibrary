1fc5fb09defa491c14167fd9fbad283604e0e111
Valery Efremov <valeriy131100@gmail.com>
2022-02-02 10:21:06 +0300

    Download books from tululu.org

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+
+import requests
+
+
+def download_books(count, folder_path):
+    book_url = 'http://tululu.org/txt.php?id={book_id}'
+
+    for book_id in range(1, count + 1):
+        response = requests.get(
+            book_url.format(book_id=book_id)
+        )
+        response.raise_for_status()
+
+        book_filename = f'{book_id}.txt'
+        book_filepath = os.path.join(folder_path, book_filename)
+        with open(book_filepath, 'wb') as file:
+            file.write(response.content)
+
+
+os.makedirs('books', exist_ok=True)
+download_books(10, 'books')

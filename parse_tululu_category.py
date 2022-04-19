29d35733f1be1b4bfb50a4f2709f7898e7a16e8e
Valery Efremov <valeriy131100@gmail.com>
2022-04-19 09:39:47 +0300

    Change getting category pages range

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -12,7 +12,7 @@ def parse_category_page(category_page):
 
     books = soup.select('.d_book')
 
-    return [
+    ids = [
         int(''.join(filter(
             str.isdigit,
             book.select_one('a').attrs['href']
@@ -20,30 +20,32 @@ def parse_category_page(category_page):
         for book in books
     ]
 
+    last_page = int(soup.select('.npage')[-1].text)
+    selected_page = int(soup.select_one('.npage_select').text)
 
-def get_range(start, end):
-    if start and end:
-        return range(start, end)
-    elif end:
-        return range(1, end)
-    elif start:
-        return count(start)
-    else:
-        raise ValueError
+    return {
+        'ids': ids,
+        'last_page': max(last_page, selected_page)
+    }
 
 
 def get_category_book_ids(category_id, start, end):
     ids = []
     base_url = f'https://tululu.org/l{category_id}/'
 
-    for page in get_range(start, end):
+    for page in count(start=start):
         try:
             page_url = urljoin(base_url, f'{page}/')
             response = requests.get(page_url)
             response.raise_for_status()
             check_for_redirect(response)
 
-            ids.extend(parse_category_page(response.text))
+            parsed_page = parse_category_page(response.text)
+            ids.extend(parsed_page['ids'])
+
+            if (end and page == end) or (page == parsed_page['last_page']):
+                break
+
         except requests.HTTPError:
             break
 
